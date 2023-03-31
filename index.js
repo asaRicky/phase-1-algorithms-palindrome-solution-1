7f7c98e1c5318da3d1d87873b291059768d160f7
your name <your-email@foo.com>
2023-03-31 08:27:58 +0300

    completed

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+  // create an array from the input string
+  const wordArray = word.split("");
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join("");
+  return reversedWord;
 }
 
-/* 
+function isPalindrome(word) {
+  // Write your algorithm here
+  const reversedWord = reverseString(word);
+  if (word === reversedWord) {
+    return true;
+  }else {
+    return false;
+  }
+}
+
+/*
   Add your pseudocode here
 */
 
 /*
   Add written explanation of your solution here
+
+  when writing this code if the string of word is a palindrome it returns true
+  if the string of words is not a palindrome it should return a false
 */
 
 // You can run `node index.js` to view these console logs
